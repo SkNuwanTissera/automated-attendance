3d7524ca3636d713a924c3d304d5292c31c0c21d
sknuwantissera <airtel1@#>
2020-01-25 13:45:14 +0530

    Initial commit

---
+++ src/main/java/com/cs/aws/automated_attendance/AutomatedAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.cs.aws.automated_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedAttendanceApplication.class, args);
+    }
+
+}
