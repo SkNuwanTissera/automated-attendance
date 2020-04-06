6fb06faa51421f804f1f687f596407d90a782f49
lahiru_b <lahiru_b@epiclanka.net>
2020-04-06 13:43:25 +0530

    [Angular] Camera integration to UI, Student Marking attendance

---
+++ src/main/java/com/cs/aws/automated_attendance/dto/FaceFileDto.java
@@ -0,0 +1,14 @@
+package com.cs.aws.automated_attendance.dto;
+
+public class FaceFileDto {
+
+  private String  imagedataURl;
+
+    public String getImagedataURl() {
+        return imagedataURl;
+    }
+
+    public void setImagedataURl(String imagedataURl) {
+        this.imagedataURl = imagedataURl;
+    }
+}
